6c66746c5ecc1ea5bea1f6f30ee87c71a457014d
hassaanbk <90360809+hassaanbk@users.noreply.github.com>
2023-04-20 13:11:23 -0400

    containerizing for deployment

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM node:18-alpine
+WORKDIR /app
+COPY package*.json ./
+RUN yarn install
+
+
+COPY . .
+EXPOSE 5000
+
+CMD ["yarn", "run", "start", "--production"]
---
+++ config/env/productionEnv.js
@@ -0,0 +1,6 @@
+module.exports = {
+    db: 'mongodb+srv://hassaanbk:BweptzoJoEBvu03y@cluster0.nzwvgvz.mongodb.net/?retryWrites=true&w=majority/hospitaldb',
+    sessionSecret: 'developmentSessionSecret',
+    secretKey: 'real_secret',
+    jwtExpiry: 300
+}
